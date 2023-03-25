770f10bb69621ee91298f6c11a73ed0778c55f23
Tim-Tim-Tim <timgs2002@gmail.com>
2023-03-25 18:52:43 -0400

    more Player

--- Hackathon/Player.js
+++ Hackathon/Player.js
@@ -5,6 +5,20 @@ class Player{
             y: 100,
         }
 
+        this.width = 50,
+        this.height = 50,
+
+        this.velocity = 0;
+
+        this.gravity = 0;
+
+        this.sides = {
+            bottom: this.height + this.position.y,
+        }
+
+    }
+
+    draw(){
         
     }
 }
