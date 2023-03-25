68a3cb832874256b414a74eab829e21347f668be
Josh Needles <jneedles@uncc.edu>
2023-03-25 18:56:03 -0400

    added attack box

--- Hackathon/Index.js
+++ Hackathon/Index.js
@@ -17,4 +17,34 @@ function animate(){
 
 }
 
+
 animate();
+
+class Sprite{
+    constructor({ position, velocity})
+    {
+        this.position = position
+        this.velocity = velocity
+        this.height = 150
+        this.attackBox = {
+            position: this.position,
+            width: 100,
+            height: 50,
+        }
+    }
+}
+draw() 
+{
+    c.fillStyle = 'red'
+    c.fillRect(this.position.x, this.position.y, 50, this.height)
+
+// attack box drawn here 
+    c.fillStyle = 'green'
+    c.fillRect
+    (
+        this.attackBox.position.x, 
+        this.attackBox.position.y, 
+        this.attackBox.width,
+        this.attackBox.height
+    )
+}
